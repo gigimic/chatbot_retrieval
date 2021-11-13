3318259c3e50ce2ef2953c57b3576ac392e737c7
gigimic <gigimic@gmail.com>
2021-11-13 11:56:52 +0530

    update

--- train_chatbot.py
+++ train_chatbot.py
@@ -5,9 +5,9 @@ import json
 import pickle
 
 import numpy as np
-# from keras.models import Sequential
-# from keras.layers import Dense, Activation, Dropout
-# from keras.optimizers import SGD
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
 import random
 
 words=[]
@@ -83,21 +83,21 @@ print(len(train_x), len(train_y))
 print(train_x[0])
 print(train_y[0])
 
-# # Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
-# # equal to number of intents to predict output intent with softmax
-# model = Sequential()
-# model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
-# model.add(Dropout(0.5))
-# model.add(Dense(64, activation='relu'))
-# model.add(Dropout(0.5))
-# model.add(Dense(len(train_y[0]), activation='softmax'))
+# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
+# equal to number of intents to predict output intent with softmax
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
 
-# # Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
-# sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-# model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 # #fitting and saving the model 
 # hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
 # # model.save('chatbot_model.h5', hist)
 
-# print("model created")
+print("model created")
